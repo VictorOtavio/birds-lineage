b09efcad09a255c0b078ac7e341b7f29b72f3a0d
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-05-07 18:09:07 -0300

    cages final touches

--- resources/views/repertories/cages/form.blade.php
+++ resources/views/repertories/cages/form.blade.php
@@ -1,54 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="section">
-  <div class="card">
-    <div class="card-header has-background-primary">
-      <div class="card-header-title has-text-white">
-        {{ __('Repertories') }}
-        <span class="icon">
-          <i class="fas fa-angle-double-right"></i>
-        </span>
-        {{ __('Cages') }}
-        <span class="icon">
-          <i class="fas fa-angle-double-right"></i>
-        </span>
+<nav class="breadcrumb has-margin-top-24">
+  <ul>
+    <li>
+      <a href="{{ route('repertories:cages.index') }}">{{ __('Repertories') }}</a>
+    </li>
+    <li>
+      <a href="{{ route('repertories:cages.index') }}">{{ __('Cages') }}</a>
+    </li>
+    <li class="is-active">
+      @if (isset($cage))
+        <a href="{{ route('repertories:cages.edit', $cage->id) }}">{{ __('Update cage') }}</a>
+      @else
+        <a href="{{ route('repertories:cages.create') }}">{{ __('Add new cage') }}</a>
+      @endif
+    </li>
+  </ul>
+</nav>
+
+<div class="card">
+  <div class="card-header has-background-primary">
+    <div class="card-header-title has-text-white">
+      @if (isset($cage))
+        {{ __('Update cage') }}
+      @else
         {{ __('Add new cage') }}
-      </div>
-    </div>
-
-    <div class="card-content">
-      <form action="{{ route('repertories:cages.store') }}" method="post">@csrf
-        <div class="field">
-          <label for="cage_number" class="label">
-            {{ __('Number') }}
-          </label>
-          <div class="control">
-            <input
-              id="cage_number"
-              type="text"
-              name="number"
-              value="{{ isset($cage) ? $cage['number'] : old('number') }}"
-              class="input @error('number') is-danger @enderror"
-              placeholder="{{ __('Cage number') }}"
-              required
-            >
-          </div>
-          @error('number')
-            <p class="help is-danger">{{ $message }}</p>
-          @enderror
-        </div>
-
-        <div class="field is-grouped">
-          <div class="control">
-            <x-save-button />
-          </div>
-          <div class="control">
-            <x-back-to-index-button url="{{ route('repertories:cages.index') }}" />
-          </div>
-        </div>
-      </form>
+      @endif
     </div>
   </div>
-</section>
+
+  <div class="card-content">
+    <form action="{{ route('repertories:cages.store') }}" method="post">
+      @csrf
+
+      <div class="field">
+        <label for="cage_number" class="label">
+          {{ __('Number') }}
+        </label>
+        <div class="control">
+          <input
+            id="cage_number"
+            type="text"
+            name="number"
+            value="{{ isset($cage) ? $cage['number'] : old('number') }}"
+            class="input @error('number') is-danger @enderror"
+            placeholder="{{ __('Cage number') }}"
+            required
+          >
+        </div>
+        @error('number')
+          <p class="help is-danger">{{ $message }}</p>
+        @enderror
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <x-save-button />
+        </div>
+        <div class="control">
+          <x-back-to-index-button url="{{ route('repertories:cages.index') }}" />
+        </div>
+      </div>
+
+    </form>
+  </div>
+</div>
 @endsection
--- resources/views/repertories/cages/index.blade.php
+++ resources/views/repertories/cages/index.blade.php
@@ -1,65 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="section">
-  <div class="card">
-    <div class="card-header has-background-primary">
-      <div class="card-header-title has-text-white">
-        {{ __('Repertories') }}
-        <span class="icon">
-          <i class="fas fa-angle-double-right"></i>
-        </span>
-        {{ __('Cages') }}
-      </div>
-    </div>
+<nav class="breadcrumb has-margin-top-24">
+  <ul>
+    <li>
+      <a href="{{ route('repertories:cages.index') }}">{{ __('Repertories') }}</a>
+    </li>
+    <li class="is-active">
+      <a href="{{ route('repertories:cages.index') }}">{{ __('Cages') }}</a>
+    </li>
+  </ul>
+</nav>
 
-    <div class="card-content">
-      <div class="buttons has-addons is-right">
-        <a class="button is-link is-small" href="{{ route('repertories:cages.create') }}">
-          <span class="icon"><i class="fas fa-plus"></i></span>
-          <span>{{ __('Add New') }}</span>
-        </a>
-      </div>
-
-      @if (!$cages->isEmpty())
-        <ul class="block-list">
-          @foreach ($cages as $cage)
-            <li class="is-clearfix">
-              {{ __('Cage number') }}: <strong>{{ $cage['number'] }}</strong>
-              <div class="is-pulled-right">
-                <a
-                  href="{{ route('repertories:cages.edit', $cage['id']) }}"
-                  class="button is-link is-small"
-                  title="{{ __('Edit') }}"
-                >
-                  <span class="icon">
-                    <i class="fas fa-edit"></i>
-                  </span>
-                </a>
-                <a
-                  href="{{ route('repertories:cages.destroy', $cage['id']) }}"
-                  class="button is-danger is-small"
-                  title="{{ __('Remove') }}"
-                >
-                  <span class="icon">
-                    <i class="fas fa-times"></i>
-                  </span>
-                </a>
-              </div>
-            </li>
-          @endforeach
-        </ul>
-      @else
-      <p class="has-text-centered">
-        <span class="icon has-text-warning">
-          <i class="fas fa-exclamation-triangle"></i>
-        </span>
-        <span class="has-text-grey-light">
-          {{ __('Nothing found') }}
-        </span>
-      </p>
-      @endif
+<div class="card">
+  <div class="card-header has-background-primary">
+    <div class="card-header-title has-text-white">
+      {{ __('Cages list') }}
     </div>
   </div>
-</section>
+
+  <div class="card-content">
+    <div class="buttons has-addons is-right">
+      <a class="button is-link is-small" href="{{ route('repertories:cages.create') }}">
+        <span class="icon"><i class="fas fa-plus"></i></span>
+        <span>{{ __('Add New') }}</span>
+      </a>
+    </div>
+
+    @if (!$cages->isEmpty())
+      <ul class="block-list">
+        @foreach ($cages as $cage)
+          <li class="is-clearfix">
+            {{ __('Cage number') }}: <strong>{{ $cage['number'] }}</strong>
+            <div class="is-pulled-right">
+              <a
+                href="{{ route('repertories:cages.edit', $cage['id']) }}"
+                class="button is-link is-small"
+                title="{{ __('Edit') }}"
+              >
+                <span class="icon">
+                  <i class="fas fa-edit"></i>
+                </span>
+              </a>
+              <a
+                href="{{ route('repertories:cages.destroy', $cage['id']) }}"
+                class="button is-danger is-small"
+                title="{{ __('Remove') }}"
+              >
+                <span class="icon">
+                  <i class="fas fa-times"></i>
+                </span>
+              </a>
+            </div>
+          </li>
+        @endforeach
+      </ul>
+    @else
+    <p class="has-text-centered">
+      <span class="icon has-text-warning">
+        <i class="fas fa-exclamation-triangle"></i>
+      </span>
+      <span class="has-text-grey-light">
+        {{ __('Nothing found') }}
+      </span>
+    </p>
+    @endif
+  </div>
+</div>
 @endsection
